986a6cbbb4921a705df1829ea86526eea4c236a1
razan1994alali <razan.alali1994@gmail.com>
2018-07-29 22:11:06 +0300

    Initial Commit

---
+++ NotificationTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotificationTest
+//
+//  Created by razan al ali on 7/29/18.
+//  Copyright © 2018 razan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
